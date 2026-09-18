7c14195f523330914c0c3d402cfae1a77ef331b0
Cklla <contact@cklla.ch>
2026-09-18 21:23:27 +0200

    chore: versionCode 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "fr.cklla.pellicule"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
