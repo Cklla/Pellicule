494d2aeb614a9ca02b812bb418b41dcfc741754f
Cklla <contact@cklla.ch>
2026-09-18 20:52:02 +0200

    versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "fr.cklla.pellicule"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
